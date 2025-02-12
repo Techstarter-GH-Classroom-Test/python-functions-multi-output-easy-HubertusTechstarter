63bba9ef64d0d3cd84b8f4b9396fc618e5379d4b
HubertusTechstarter <152187829+HubertusTechstarter@users.noreply.github.com>
2025-02-12 16:05:02 +0100

    Update exercises.py

--- exercises.py
+++ exercises.py
@@ -1,9 +1,9 @@
-# Aufgabe 1: Berechnet den Durchschnitt und die Anzahl der Elemente eines Zahlenarrays
+# Aufgabe 1: Berechnet den Durchschnitt und die Anzahl der Elemente eines Arrays mit Zahlen
 def calc_1(array):
-    if not array:  # Überprüfen, ob das Array leer ist, um Division durch Null zu vermeiden
-        return None, 0
+    if not array:  # Falls das Array leer ist, geben wir 0 als Durchschnitt zurück
+        return 0, 0
     return sum(array) / len(array), len(array)
 
-# Aufgabe 2: Gibt die Summe und das Produkt von zwei Zahlen zurück
+# Aufgabe 2: Nimmt zwei Parameter entgegen und gibt die Summe und das Produkt zurück
 def calc_2(n, m):
     return n + m, n * m
